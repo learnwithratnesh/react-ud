51971a6e39f079fe75d064eec11bcd2768ffba8c
Ratnesh Tiwari <ratneshtanj26@gmail.com>
2021-07-23 19:57:24 +0530

    use state

--- src/App.js
+++ src/App.js
@@ -23,9 +23,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = (expense) => {
+    console.log(expense)
+  }
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={_expenses}></Expenses>
     </div>
   );
